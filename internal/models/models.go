20645b006956e9411e70ba152dba2a196f03871c
Dimix-international <dima.dimix94@mail.ru>
2024-05-04 16:07:00 +0300

    parserAnalyzer: model for commands

--- internal/models/models.go
+++ internal/models/models.go
@@ -2,6 +2,24 @@ package models
 
 import "errors"
 
+const (
+	GetCommand     = "GET"
+	SetCommand     = "SET"
+	DeleteCommand  = "DELETE"
+	UnknownCommand = "UNKNOWN"
+
+	GetCommandArgumentsNumber    = 1
+	DeleteCommandArgumentsNumber = 1
+	SetCommandArgumentsNumber    = 2
+)
+
 var (
-	ErrParsing = errors.New("invalid argument for parsing")
+	CommandRatioWithArument = map[string]int{
+		GetCommand:    GetCommandArgumentsNumber,
+		SetCommand:    SetCommandArgumentsNumber,
+		DeleteCommand: DeleteCommandArgumentsNumber,
+	}
+	ErrParsing          = errors.New("invalid argument for parsing")
+	ErrInvalidArguments = errors.New("invalid arguments")
+	ErrInvalidCommand   = errors.New("invalid command")
 )
