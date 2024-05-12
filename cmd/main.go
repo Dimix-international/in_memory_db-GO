e2788f18f85aa34f43a262cda8b54e3e07d5b762
Dimix-international <dima.dimix94@mail.ru>
2024-05-12 11:30:39 +0300

    dev: refactor

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/Dimix-international/in_memory_db-GO/db"
 	"github.com/Dimix-international/in_memory_db-GO/internal/app"
 	"github.com/Dimix-international/in_memory_db-GO/internal/config"
@@ -12,10 +18,30 @@ import (
 func main() {
 	cfg := config.MustLoadConfig()
 	log := logger.SetupLogger(cfg.Env)
-	app.NewApp(
+	app := app.NewApp(
 		log,
 		handler.NewHanlderMessages(log, service.NewParserService(), service.NewAnalyzerService(log), db.NewShardMap(10)),
-	).Run()
+	)
 
-	log.Info("app finish")
+	exit := make(chan os.Signal, 1)
+	signal.Notify(exit, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		if err := app.Run(); err != nil {
+			log.Error("Stop app", "err", err)
+			exit <- syscall.SIGTERM
+			close(exit)
+		}
+	}()
+
+	<-exit
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownCancel()
+
+	if err := app.ShutdownApp(shutdownCtx); err != nil {
+		log.Error("error closing app", "err", err)
+	}
+
+	log.Info("program finish")
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -5,10 +5,7 @@ import (
 	"context"
 	"log/slog"
 	"os"
-	"os/signal"
 	"strings"
-	"syscall"
-	"time"
 
 	"github.com/Dimix-international/in_memory_db-GO/internal/handler"
 	"github.com/Dimix-international/in_memory_db-GO/internal/models"
@@ -29,31 +26,8 @@ func NewApp(log *slog.Logger, handlerMessage *handler.HandlerMessages) *App {
 	}
 }
 
-// Run launches app
-func (s *App) Run() {
-	s.log = s.log.With(slog.String("op", "app"))
-	exit := make(chan os.Signal, 1)
-	signal.Notify(exit, os.Interrupt, syscall.SIGTERM)
-
-	go func() {
-		if err := s.launchApp(); err != nil {
-			s.log.Error("Stop server", "err", err)
-			exit <- syscall.SIGTERM
-			close(exit)
-		}
-	}()
-
-	<-exit
-
-	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer shutdownCancel()
-
-	if err := s.shutdown(shutdownCtx); err != nil {
-		s.log.Error("error closing app", "err", err)
-	}
-}
-
-func (s *App) launchApp() error {
+// Run app
+func (s *App) Run() error {
 	reader := bufio.NewReader(os.Stdin)
 	s.log.Info("start app")
 
@@ -73,7 +47,7 @@ func (s *App) launchApp() error {
 	}
 }
 
-func (s *App) shutdown(ctx context.Context) error {
+func (s *App) ShutdownApp(ctx context.Context) error {
 	for _, fn := range s.closers {
 		if err := fn(ctx); err != nil {
 			return err
